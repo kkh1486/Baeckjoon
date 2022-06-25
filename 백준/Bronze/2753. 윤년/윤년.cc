28adb880f9dd9deccec8d14a1707bab23bd4483b
kkh1486 <102204603+kkh1486@users.noreply.github.com>
2022-06-25 21:47:22 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int year;
+
+	cin >> year;
+	if (year > 4000 || year < 1) {
+		cout << "1 <= year <= 4000";
+		return 0;
+	}
+	else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+		cout << "1";
+	}
+	else {
+		cout << "0";
+	}
+
+	return 0;
+}
