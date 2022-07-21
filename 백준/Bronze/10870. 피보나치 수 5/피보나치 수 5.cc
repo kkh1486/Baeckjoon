cef568bf7d52de1a52dfda3904d7691118758e0d
kkh1486 <102204603+kkh1486@users.noreply.github.com>
2022-07-21 20:07:30 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N = 0;
+
+    cin >> N;
+    cout << fibo(N);
+
+    return 0;
+}
