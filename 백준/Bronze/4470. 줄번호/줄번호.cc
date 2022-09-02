1bf3420eae19de6d48fb0a16eb6d388ddc97487c
kkh1486 <102204603+kkh1486@users.noreply.github.com>
2022-09-03 03:01:22 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int N = 0;
+    string str;
+
+    cin >> N;
+    cin.ignore();
+    for (int i = 1; i <= N; i++) {
+        getline(cin, str);
+        cout << i << ". " << str << "\n";
+    }
+
+    return 0;
+}
