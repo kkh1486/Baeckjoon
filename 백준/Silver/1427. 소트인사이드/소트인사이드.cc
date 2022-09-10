9d7d32b00e1f5f53c166bb96dddd60f09284f583
kkh1486 <102204603+kkh1486@users.noreply.github.com>
2022-09-09 23:49:11 -0700

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string N;
+
+    cin >> N;
+    sort(N.begin(), N.end(), greater<char>());
+    cout << N;
+
+    return 0;
+}
